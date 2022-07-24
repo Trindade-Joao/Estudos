169e425f452e8bdcfa5ae4de91cce16419663374
Trindade-Joao <joao.a.trindade.jr@gmail.com>
2022-07-23 23:27:35 -0300

    initial commit

---
+++ js/login.js
@@ -0,0 +1,8 @@
+const input = document.querySelector('.login_input');
+const button = document.querySelector('.login_button');
+
+const validacaoInput = (event) => {
+    
+}
+
+input.addEventListener('input', validacaoInput);

a3a209c4a52b57788212d1a675120b1c27dc62b8
Trindade-Joao <joao.a.trindade.jr@gmail.com>
2022-07-24 00:05:14 -0300

    pagina de login finalizada

--- js/login.js
+++ js/login.js
@@ -1,8 +1,21 @@
 const input = document.querySelector('.login_input');
 const button = document.querySelector('.login_button');
+const form = document.querySelector('.login-form');
 
-const validacaoInput = (event) => {
-    
+const validacaoInput = ({ target }) => {
+    if(target.value.length > 2){
+        button.removeAttribute('disabled');
+    } else {
+        button.setAttribute('disabled', '');
+    }
+}
+
+const clicandoEnviar = (event) => {
+    event.preventDefault();    
+
+    localStorage.setItem('player', input.value);
+    window.location = 'pages/game.html';
 }
 
 input.addEventListener('input', validacaoInput);
+form.addEventListener('submit', clicandoEnviar);
